e9063d6a11c7211c83382923538743da013fcf1f
MiriamDiaz2306 <miriam.diaz1008@icloud.com>
2023-03-13 23:33:50 -0500

    Readme listo

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const {
     getLinks, 
     getLinkStatus,
 } = require('./functions');
-
+//verifica la existencia del archivo especificado en la ruta y rechaza la promesa si el archivo no existe. 
 const mdLinks = (path, options) => new Promise((resolve, reject) => { //path (la ruta del archivo Markdown) y options (opciones para personalizar el comportamiento de la función).
   if (!pathExists(path)) {
     reject(new Error('Path does not exist'));
     return;
   }
-  const pathAbsolute = turnPathAbsolute(path);
-  if (!isExtensionMd(pathAbsolute)) {
+  const pathAbsolute = turnPathAbsolute(path);//espera que sea la ruta del archivo Md y la convierte absoluta si no era
+  if (!isExtensionMd(pathAbsolute)) {//verifica si el archivo es un md
     reject(new Error('Path is not an extension file .md'));
     return;
   }
-  getLinks(pathAbsolute).then((arrayLinks) => {
-    if (arrayLinks.length === 0) {
+  getLinks(pathAbsolute).then((arrayLinks) => { // devuelva un array de objetos con la información de los links encontrados en el archivo Md.
+    if (arrayLinks.length === 0) { //si es una longitud de 0 ( no se encontraron links en el archivo), la promesa se rechaza 
       reject(new Error('Path does not have links'));
       return;
     }
@@ -25,8 +25,8 @@ const mdLinks = (path, options) => new Promise((resolve, reject) => { //path (la
       resolve(arrayLinks);
       return;
     }
-    getLinkStatus(arrayLinks).then((response) => {
-      resolve(response);
+    getLinkStatus(arrayLinks).then((response) => {//espera que devuelva un array de objetos con la información de los links,
+      resolve(response); // resuelve la promesa original de "mdLinks" con el array de objetos con la información de los links, incluyendo su estado HTTP
     });
   });
 });
